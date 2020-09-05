a06f5173576ae7f43f9ec6406d6c5f6ef2aefd19
Kondo Taiki <taiki-k@users.noreply.github.com>
2020-09-05 10:27:44 +0900

    GPLv3ライセンスの表示をdocstringからブロックコメントに変更

--- time_record.sql
+++ time_record.sql
@@ -19,14 +19,14 @@
 
 -- This table is for recording elapse time each queries.
 
-drop table if exists time_record cascade;
+DROP TABLE IF EXISTS time_record CASCADE;
 
-create table time_record (
-  global_start text        not null,
-  client_no    integer     not null,
-  loop_count   integer     not null,
-  query_no     text        not null,
-  start_time   timestamptz not null,
+CREATE TABLE time_record (
+  global_start text        NOT NULL,
+  client_no    integer     NOT NULL,
+  loop_count   integer     NOT NULL,
+  query_no     text        NOT NULL,
+  start_time   timestamptz NOT NULL,
   end_time     timestamptz,
   result       jsonb,
   PRIMARY KEY (global_start, client_no, loop_count, query_no)
